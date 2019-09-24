721a4c4399ed77eca0c106e1217d08d7d9f2b25f
Ben <wuchienhsun01@gmail.com>
2019-09-24 23:39:32 +0800

    first to text aws codeDeploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          now is feature branch <code>src/App.js</code> 
+          practice aws codeDeploy <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
